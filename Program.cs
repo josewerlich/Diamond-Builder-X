f87a802e42e0759044e147ce3bf5206db0faa767
josewerlich <werlich@outlook.com>
2025-04-08 20:23:18 -0400

    Menu creation and user input validation to accept only odd numbers

--- Program.cs
+++ Program.cs
@@ -2,10 +2,44 @@
 {
     internal class Program
     {
-
+        //Version 1: Menu creation and user input validation to accept only odd numbers.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int diamondSize = 0;
+
+
+            while (true)
+            {
+                Console.WriteLine("************************************");
+                Console.WriteLine("********* Diamond Creator **********");
+                Console.WriteLine("************************************");
+
+                Console.WriteLine();
+
+                Console.Write("Type the size of the diamond you want to create: ");
+                diamondSize = Console.Read();
+
+                if (diamondSize % 2 != 0)
+                {
+                    
+                    Console.WriteLine("The number is odd, press Enter to continue");
+                    Console.ReadLine();
+                }
+
+                else
+                {
+                    Console.WriteLine("Please type a odd number. Press Enter to restart");
+                    Console.ReadLine();
+
+                }
+                Console.ReadLine();
+            }
+
+
+            
+
+
         }
+
     }
 }
