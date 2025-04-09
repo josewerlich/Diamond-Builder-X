0f36587dfd3fd58b521a3285206d84d337dc9aa8
josewerlich <werlich@outlook.com>
2025-04-08 20:46:34 -0400

    Print the middle line of the diamond based on the user input

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿namespace Diamond_Builder_X
+﻿using System.Diagnostics.Metrics;
+
+namespace Diamond_Builder_X
 {
     internal class Program
     {
-        //Version 1: Menu creation and user input validation to accept only odd numbers.
+        //Version 2: Print the middle line of the diamond based on the user input
         static void Main(string[] args)
         {
             int diamondSize = 0;
@@ -17,13 +19,17 @@
                 Console.WriteLine();
 
                 Console.Write("Type the size of the diamond you want to create: ");
-                diamondSize = Console.Read();
+                diamondSize = Convert.ToInt32(Console.ReadLine());
 
                 if (diamondSize % 2 != 0)
                 {
-                    
-                    Console.WriteLine("The number is odd, press Enter to continue");
-                    Console.ReadLine();
+                    char[] diamondMiddleLine = new char[diamondSize];
+
+                    for (int counter = 0; counter < diamondSize; counter++ )
+                    {
+                        diamondMiddleLine[counter] = 'X';
+                    }
+                    Console.WriteLine(new string (diamondMiddleLine)); 
                 }
 
                 else
