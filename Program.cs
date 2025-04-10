b9d46c40ce5597c893a3ee3681fd4188af506e45
josewerlich <werlich@outlook.com>
2025-04-10 19:37:55 -0400

    Version 4: Refactoring the code and adding an option to create a new or close the program

--- Program.cs
+++ Program.cs
@@ -1,25 +1,19 @@
 ﻿using System.Diagnostics.Metrics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Diamond_Builder_X
 {
     internal class Program
     {
-        //Version 3: Print the top half and the bottom half
+        //Version 4: Refactoring the code and adding an option to create a new or close the program
         static void Main(string[] args)
         {
-            int number = 0;
-
 
             while (true)
             {
-                Console.WriteLine("************************************");
-                Console.WriteLine("********* Diamond Creator **********");
-                Console.WriteLine("************************************");
+                ShowMenu();
 
-                Console.WriteLine();
-
-                Console.Write("Type the size of the diamond you want to create: ");
-                number = Convert.ToInt32(Console.ReadLine());
+                int number = NumberInput();
 
                 if (number % 2 != 0)
                 {
@@ -28,38 +22,74 @@ namespace Diamond_Builder_X
 
 
                     //top half
-                    for (int counter = 0; counter < diamondHalfSize; counter++)
-                    {
-                        Console.Write(new string(' ', diamondHalfSize - counter));
-                        Console.WriteLine(new string('*', counter * 2 + 1));
-                    }
-                    
+                    ShowTopHalfDiamond(diamondHalfSize);
+
                     //center line
-                    Console.WriteLine(new string('*', number));
-                    
-             
+                    ShowTheCenterLineDiamond(number);
+
+
                     //bottom half
-                    for (int counter = diamondHalfSize -1; counter >= 0; counter--)
-                    {
-                        Console.Write(new string(' ', diamondHalfSize - counter));
-                        Console.WriteLine(new string('*', counter * 2 + 1));
-                    }
+                    ShowBottomHalfDiamond(diamondHalfSize);
                 }
 
                 else
                 {
                     Console.WriteLine("Please type a odd number. Press Enter to restart");
-                    
-
                 }
-                Console.ReadLine();
-            }
+                string continueOption = MenuDisplayContinue();
 
-            
-            
+                if (continueOption != "Y")
+                    break;
+
+            }
 
 
         }
+        static void ShowMenu()
+        {
+            Console.WriteLine("************************************");
+            Console.WriteLine("********* Diamond Creator **********");
+            Console.WriteLine("************************************");
 
+            Console.WriteLine();
+        }
+        static int NumberInput()
+        {
+            Console.Write("Type the size of the diamond you want to create: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            return number;
+        }
+        static void ShowTopHalfDiamond(int diamondHalfSize)
+        {
+            for (int counter = 0; counter < diamondHalfSize; counter++)
+            {
+                Console.Write(new string(' ', diamondHalfSize - counter));
+                Console.WriteLine(new string('X', counter * 2 + 1));
+            }
+        }
+
+        static void ShowTheCenterLineDiamond(int number)
+        {
+            Console.WriteLine(new string('X', number));
+        }
+
+        static void ShowBottomHalfDiamond(int diamondHalfSize)
+        {
+            for (int counter = diamondHalfSize - 1; counter >= 0; counter--)
+            {
+                Console.Write(new string(' ', diamondHalfSize - counter));
+                Console.WriteLine(new string('X', counter * 2 + 1));
+            }
+        }
+
+        static string MenuDisplayContinue()
+        {
+            Console.WriteLine("***********************************");
+            Console.WriteLine("Do you want create a new diamond? (Y/N) ");
+
+            string continueOption = Console.ReadLine()!.ToUpper();
+
+            return continueOption;
+        }
     }
 }
